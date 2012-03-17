1351fa937155e03305a0cd88659d48ca63a5c31e
Steven Rhodes <rhodes.steven@gmail.com>
2012-03-17 02:08:37 -0700

    More playing around with wx

--- Python/gui.py
+++ Python/gui.py
@@ -4,6 +4,7 @@
 
 import wx
 import wx.combo
+import wx.lib.buttons as buttons
 from wx.lib.wordwrap import wordwrap
 import os
 import Image
@@ -26,8 +27,12 @@ class MainWindow(wx.Frame):
     menubar.Append(fileMenu, "&File")
     
     viewMenu = wx.Menu()
-    fxyplot= viewMenu.AppendRadioItem(wx.ID_ANY, "XY Plot", " Do an XY Plot on the machine")
-    fcalib = viewMenu.AppendRadioItem(wx.ID_ANY, "Calibrate", " Calibrate the machine")
+    fcalib = viewMenu.AppendRadioItem(wx.ID_ANY, "Calibrate",
+                                      " Calibrate the machine")
+    fxyplot= viewMenu.AppendRadioItem(wx.ID_ANY, "XY Plot",
+                                      " Do an XY Plot on the machine")
+    fmesh = viewMenu.AppendRadioItem(wx.ID_ANY, "3-D Drawing",
+                                     " Use all 5 dimensions to paint")
     menubar.Append(viewMenu, "&Window")
 
     self.SetMenuBar(menubar)
@@ -35,41 +40,56 @@ class MainWindow(wx.Frame):
     self.sb = self.CreateStatusBar()
     self.sb.SetFieldsCount(2)
     self.sb.SetStatusWidths([-5,-2])
-    self.sb.SetStatusText('Hello, World!', 1);
+    self.sb.SetStatusText('Hello, World!', 1)
     
     self.Bind(wx.EVT_MENU, self.OnAbout, fabout)
     self.Bind(wx.EVT_MENU, self.OnQuit, fquit)
     self.Bind(wx.EVT_MENU, self.OnXYPlot, fxyplot)
     self.Bind(wx.EVT_MENU, self.OnCalibrate, fcalib)
+    self.Bind(wx.EVT_MENU, self.OnMesh, fmesh)
     
     
   def InitUI(self):
     """Initialize the primary window"""
     self.InitMenuAndStatus()
 
-    self.xypanel = XYPanel(self)
-    self.xypanel.Show()
     self.calibratepanel = CalibratePanel(self)
-    self.calibratepanel.Hide()
+    self.calibratepanel.Show()
+    self.xypanel = XYPanel(self)
+    self.xypanel.Hide()
+    self.meshpanel = MeshPanel(self)
+    self.meshpanel.Hide()
     self.sizer = wx.BoxSizer(wx.VERTICAL)
-    self.sizer.Add(self.xypanel, 1, wx.EXPAND)
     self.sizer.Add(self.calibratepanel, 1, wx.EXPAND)
+    self.sizer.Add(self.xypanel, 1, wx.EXPAND)
+    self.sizer.Add(self.meshpanel, 1, wx.EXPAND)
     self.SetSizer(self.sizer)
     self.SetTitle("XY Plot")    
+    self.SetSize((500,800))
     self.Centre()
-  
-  def OnXYPlot(self, e):
-    self.SetTitle("XY Plot")
-    self.xypanel.Show()
-    self.calibratepanel.Hide()
-    self.xypanel.SetFocus()
-    self.Layout()
   
   def OnCalibrate(self, e):
     self.SetTitle("Calibrate")
-    self.xypanel.Hide()
     self.calibratepanel.Show()
+    self.xypanel.Hide()
+    self.meshpanel.Hide()
     self.calibratepanel.SetFocus()
+    self.Layout()
+    
+  def OnXYPlot(self, e):
+    self.SetTitle("XY Plot")
+    self.calibratepanel.Hide()
+    self.xypanel.Show()
+    self.meshpanel.Hide()
+    self.xypanel.SetFocus()
+    self.Layout()
+  
+  def OnMesh(self, e):
+    self.SetTitle("3-D Drawing")
+    self.calibratepanel.Hide()
+    self.xypanel.Hide()
+    self.meshpanel.Show()
+    self.meshpanel.SetFocus()
     self.Layout()
   
   def OnAbout(self, e):
@@ -103,21 +123,46 @@ class MainWindow(wx.Frame):
     """Quit the program"""
     self.Close()
 
-class XYPanel(wx.Panel):
-  """The panel for plotting XY graphs"""
-  def __init__(self, parent):
-    wx.Panel.__init__(self, parent=parent)
-    self.db = wx.Button(self, 10, "A Button!", (20, 20))
-    self.fileselect = FileSelectorCombo(self, size=(150,-1))
-#    self.img = wxImage(100,100)
-
-    
-
 class CalibratePanel(wx.Panel):
   """The panel for calibrating the machine"""
   def __init__(self, parent):
     wx.Panel.__init__(self, parent=parent)
     self.parent = parent
+    
+    pb = wx.StaticBox(self, wx.ID_ANY, "Positions of Motors")
+    pSizer = wx.StaticBoxSizer(pb, wx.VERTICAL)
+    pGrid = wx.GridBagSizer(5,3)
+    self.xNum = wx.TextCtrl(self, wx.ID_ANY, "0.0")
+    self.xBut = buttons.GenButton(self, wx.ID_ANY, "Zero")
+    self.yNum = wx.TextCtrl(self, wx.ID_ANY, "0.0")
+    self.yBut = buttons.GenButton(self, wx.ID_ANY, "Zero")
+    self.zNum = wx.TextCtrl(self, wx.ID_ANY, "0.0")
+    self.zBut = buttons.GenButton(self, wx.ID_ANY, "Zero")
+    self.panNum = wx.TextCtrl(self, wx.ID_ANY, "0.0")
+    self.panBut = buttons.GenButton(self, wx.ID_ANY, "Zero")
+    self.tiltNum = wx.TextCtrl(self, wx.ID_ANY, "0.0")
+    self.tiltBut = buttons.GenButton(self, wx.ID_ANY, "Zero")
+    pGrid.AddMany([(wx.StaticText(self, wx.ID_ANY, "X:"), (0,0)),
+                   (self.xNum, (0,1)),
+                   (self.xBut, (0,2)),
+                   (wx.StaticText(self, wx.ID_ANY, "Y:"), (1,0)),
+                   (self.yNum, (1,1)),
+                   (self.yBut, (1,2)),
+                   (wx.StaticText(self, wx.ID_ANY, "Z:"), (2,0)),
+                   (self.zNum, (2,1)),
+                   (self.zBut, (2,2)),
+                   (wx.StaticText(self, wx.ID_ANY, "Pan:"), (3,0)),
+                   (self.panNum, (3,1)),
+                   (self.panBut, (3,2)),
+                   (wx.StaticText(self, wx.ID_ANY, "Tilt:"), (4,0)),
+                   (self.tiltNum, (4,1)),
+                   (self.tiltBut, (4,2))])
+    self.xNum.Enable( False )
+    self.yNum.Enable( False )
+    self.zNum.Enable( False )
+    self.panNum.Enable( False )
+    self.tiltNum.Enable( False )
+    pSizer.Add(pGrid, 1, wx.EXPAND)
     
     self.Bind(wx.EVT_KEY_DOWN, self.OnKeyDown)
     self.control = wx.TextCtrl(self, style=wx.TE_MULTILINE)
@@ -125,7 +170,7 @@ class CalibratePanel(wx.Panel):
     self.db = wx.Button(self, 10, "Default Button", (20, 20))
     self.picture = PictureChooser(self)
 
-    self.b = wx.Button(self, -1, "Create and Show an ImageDialog", (50,50))
+    self.b = wx.Button(self, wx.ID_ANY, "Create and Show an ImageDialog", (50,50))
     self.Bind(wx.EVT_BUTTON, self.OnButton, self.b)    
     
     self.vertSizer = wx.BoxSizer(wx.VERTICAL)
@@ -134,19 +179,54 @@ class CalibratePanel(wx.Panel):
     self.horizSizer.Add(self.b, 1, wx.EXPAND)
     self.vertSizer.Add(self.horizSizer, 1, wx.EXPAND)
     self.vertSizer.Add(self.control, 1, wx.EXPAND)
-    self.SetSizer(self.vertSizer)
-    self.vertSizer.Fit(self)
+    
+    
+    self.mainSizer = wx.BoxSizer(wx.HORIZONTAL)
+    self.mainSizer.Add(pSizer, 0, wx.ALL)
+    self.mainSizer.Add(self.vertSizer, 1, wx.EXPAND)
+    self.SetSizer(self.mainSizer)
+    
     
   def OnButton(self, e):
     self.parent.Close()
+ 
   def OnKeyDown(self, e):
     keycode = event.GetKeyCode()
     if keycode == wx.WXK_ESCAPE:
-      ret  = wx.MessageBox('Are you sure to quit?', 'Question', 
+      ret  = wx.MessageBox("Are you sure to quit?", 'Question', 
 		                       wx.YES_NO | wx.NO_DEFAULT, self)
       if ret == wx.YES:
          self.Close()
     event.Skip()
+
+class XYPanel(wx.Panel):
+  """The panel for plotting XY graphs"""
+  def __init__(self, parent):
+    wx.Panel.__init__(self, parent=parent)
+    self.parent = parent
+    
+    self.db = wx.Button(self, 10, "A Button!", (20, 20))
+    self.fileselect = FileSelectorCombo(self, size=(150,-1))
+    possibleColors = ["Black", "Red", "Blue", "Green"]
+    self.ch = wx.Choice(self, wx.ID_ANY, (100, 50), choices = possibleColors)
+    self.Bind(wx.EVT_CHOICE, self.ColorChange, self.ch)
+    self.ch.SetSelection(0)
+#    self.img = wxImage(100,100)
+
+  def ColorChange(self, e):
+    self.parent.sb.SetStatusText(e.GetString(), 1)
+
+class MeshPanel(wx.Panel):
+  """The panel for doing the complex mapping"""
+  def __init__(self, parent):
+    wx.Panel.__init__(self, parent=parent)
+    bmp = wx.EmptyBitmap(1,1)
+    bmp.LoadFile('bitmaps/uparrow.bmp', wx.BITMAP_TYPE_ANY)
+ 
+    mask = wx.MaskColour(bmp, wx.WHITE)
+    bmp.SetMask(mask)
+    b = wx.BitmapButton(self, -1, bmp, (20,20), (20,20))
+    
   
 class PictureChooser(wx.Panel):
   """Panel for displaying the picture you wish to paint"""
