070a24a03ab976024ac0699312bf430e4b5c9541
Steven Rhodes <rhodes.steven@gmail.com>
2013-12-01 20:13:59 -0800

    Adding gallery, software, video

--- js/cnc.js
+++ js/cnc.js
@@ -2,7 +2,7 @@ var resizeSlides = function() {
   $('.slides').css({'height':Math.round($('.slides').width()*125/160)+'px'});
 };
 var resizeVideo = function() {
-  $('#video').css({'height':Math.round($('#video').width()*9/16)+'px'});
+  $('.video').css({'height':Math.round($('.video').width()*9/16)+'px'});
 };
 $(window).resize(resizeSlides);
 $(document).ready(resizeSlides);
